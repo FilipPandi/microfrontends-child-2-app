0347bd428bde5352dd95f7cccef901ccee7574e2
filip <filip.pandi@inovatrend.com>
2023-04-17 16:01:37 +0200

    Fix mounting/unmounting, add routing test.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import './App.css'
 
+import { Image } from 'primereact/image';
+
+import {Link} from "react-router-dom";
+import {Button} from "primereact/button";
+
 function App() {
   return (
       <div style={{margin: '5%'}}>
         <Panel header="Ovo je CMS dio" className="custom-panel">
+
+            <h2><Link className={"link"} to="/com1"><Button severity="secondary" icon="pi pi-desktop" label={"Component 1"}/></Link></h2>
+            <div style={{paddingTop: "2%"}}></div>
+
           Ovo je CMS dio!
         </Panel>
+          <Image src="https://wallpapers.com/images/featured/oaax18kaapkokaro.jpg"  alt="Image" preview width="100%" />
       </div>
 
   );
--- src/index.js
+++ src/index.js
@@ -2,22 +2,55 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import * as ReactDOMClient from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Component1 from "./components/component1";
 
+let isMounted = false;
 let root = null;
 let container = null;
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "/com1",
+        element: <Component1/>,
+    },
+]);
+
 window.renderChild2 = (containerId) => {
     container = document.getElementById(containerId);
-    root = ReactDOMClient.createRoot(container);
 
-    root.render(<App/>);
+    if (isMounted === false) {
+        root = ReactDOMClient.createRoot(container);
+
+        root.render(
+            <React.StrictMode>
+                <RouterProvider router={router}/>
+            </React.StrictMode>);
+
+        console.log("Application 2 - CMS - is mounted.");
+    }
+
+    isMounted = true;
 };
 
 window.unmountChild2 = () => {
-    root.unmount(container);
+    if (isMounted === true) {
+        root.unmount(container);
+        isMounted = false;
+
+        console.log("Application 2 - CMS - unmounting previous...");
+    }
 };
 
 if (!document.getElementById('Child2-container')) {
     const container = document.getElementById('root');
     const root = ReactDOMClient.createRoot(container);
-    root.render(<App/>);
+    root.render(
+        <React.StrictMode>
+            <RouterProvider router={router}/>
+        </React.StrictMode>);
 }
